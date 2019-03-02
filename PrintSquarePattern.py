d01d43fa41271d342cb0b88fdfb473abb59e323d
Rufaidah <rufaidah.chan@gmail.com>
2019-03-02 14:41:28 +0700

    fix bug pattern persegi dengan * di ujung dan tengah berbentuk +

--- PrintSquarePattern.py
+++ PrintSquarePattern.py
@@ -10,8 +10,9 @@ else:
     for i in range (side):
         # column
         for j in range (side):
-            if i == ((side - 1) / 2) or j == ((side - 1) / 2) \
-                    or (i | j == 0 or i | j == side - 1):
+            if (i == (side - 1) / 2) or (j == (side - 1) / 2) \
+                    or (i | j == 0) or (i == 0 and j == side - 1)\
+                    or (i == side - 1 and j == 0) or (i == side - 1 and j == side - 1):
                 print("*", end='')
             else:
                 print("=", end='')
