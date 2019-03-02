0b808db363cf42b3760513f913a5df044868bc83
Rufaidah <rufaidah.chan@gmail.com>
2019-03-02 12:56:26 +0700

    pattern persegi hanya karakter * berbentuk +

--- PrintSquarePattern.py
+++ PrintSquarePattern.py
@@ -1,15 +1,19 @@
 side = int(input("Masukkan panjang sisi persegi : "))
 i = 0
+j = 0
 
 if side % 2 == 0:
     print("Panjang isi harus ganjil")
 
 else:
-    # isi baris
+    # index vertical
     for i in range (side):
-        # jumlah baris
-        for i in range (side):
-            print('*', end=' ')
+        # index horizontal
+        for j in range (side):
+            if i == ((side - 1) / 2) or j == ((side - 1) / 2):
+                print("*", end='')
+            else:
+                print("=", end='')
         print()
 
 
